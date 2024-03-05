9fcdc142320052f55076e2abd191ee9b2449dd93
dianaesthesia <bereziukdiana@gmail.com>
2024-03-05 15:43:24 +0200

    hw react lesson3 create new user

--- src/components/UsersComponent/UserForm.js
+++ src/components/UsersComponent/UserForm.js
@@ -5,11 +5,16 @@ import {userApiService} from "../../services/user.api.service";
 const UserForm = ({setUsers}) => {
     const {register, handleSubmit} = useForm();
     const createUser = (item) => {
-        userApiService.create(item).then(({data}) => setUsers(prev => [...prev, data]));
+        userApiService.create(item).then(({data}) => setUsers(prev => ([...prev, data])));
     }
     return (
         <form onSubmit={handleSubmit(createUser)}>
             <input type={'text'} placeholder={'name'} {...register('name')}/>
+            <input type={'text'} placeholder={'username'} {...register('username')}/>
+            <input type={'email'} placeholder={'email'} {...register('email')}/>
+            <input type={'text'} placeholder={'street'} {...register('address.street')}/>
+            <input type={'number'} placeholder={'phone'} {...register('phone')}/>
+
             <button>Create new user</button>
         </form>
     );
