5aaba35b5824e551522fbc283ec47b223e33a9d1
dianaesthesia <bereziukdiana@gmail.com>
2024-03-12 18:54:21 +0200

    hw react lesson3 create new user

--- src/components/UsersComponent/UserForm.js
+++ src/components/UsersComponent/UserForm.js
@@ -3,10 +3,11 @@ import {useForm} from "react-hook-form";
 import {userApiService} from "../../services/user.api.service";
 
 const UserForm = ({setUsers}) => {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, reset} = useForm();
 
     const createUser = (item) => {
         userApiService.create(item).then(({data}) => setUsers(prev => ([...prev, data])))
+        reset()
     };
 
     return (
@@ -15,7 +16,7 @@ const UserForm = ({setUsers}) => {
             <input type={'text'} placeholder={'username'} {...register('username')}/>
             <input type={'email'} placeholder={'email'} {...register('email')}/>
             <input type={'text'} placeholder={'street'} {...register('address.street')}/>
-            <input type={'number'} placeholder={'phone'} {...register('phone')}/>
+            <input type={'number'} placeholder={'phone'} {...register('phone', {valueAsNumber: true})}/>
             <button>Create new user</button>
         </form>
     );
